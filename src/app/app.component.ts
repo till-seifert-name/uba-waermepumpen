b6341cf841d7ff6caf19c3806f4ae1389e9ac22e
Till Seifert <me@till.seifert.name>
2023-05-03 11:46:44 +0200

    Implement calculator, first example questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormArray, NgForm} from '@angular/forms';
+import {NgForm} from '@angular/forms';
+import {CellContent, DataGrid, UND, WAHR, WENNS} from "./data-grid";
+import {Fragen_Prototyp_Einzelfahrzeug_data, Empfehlungslisten_data} from "./data";
+
 
 @Component({
   selector: 'app-root',
@@ -9,15 +12,98 @@ import {FormBuilder, FormGroup, Validators, FormArray, NgForm} from '@angular/fo
 export class AppComponent {
 
 
-  newCars?: number = undefined;
-  replacedCars?: number = undefined;
   publicTransport: string = '';
   bicycle: string = '';
   carPooling: string = '';
   carSharing: string = '';
   carRenting: string = '';
 
+
+  public grid: DataGrid = new DataGrid();
+
   onSubmit(form: NgForm) {
     console.log('Form data:', form.value);
   }
+
+  constructor() {
+    const grid = this.grid;
+
+    for (const [cell, content] of Object.entries(Fragen_Prototyp_Einzelfahrzeug_data)) {
+      grid.setCell("Names", cell, content);
+    }
+
+    for (const [cell, content] of Object.entries(Empfehlungslisten_data)) {
+      grid.setCell("Names", cell, content);
+    }
+
+    grid.setCell("Names", "E_A0", (sheet, cell, grid) => {
+
+      const {
+        A_JN1,
+        A_JN2,
+        A_R1,
+        A_S1,
+        A_S2,
+        A_S3,
+        A_SV1,
+        A_SV2,
+        A_SV3,
+        E_A1,
+        E_A10,
+        E_A11,
+        E_A2,
+        E_A3,
+        E_A4,
+        E_A5,
+        E_A6,
+        E_A7,
+        E_A8,
+        E_A9,
+        F_F1,
+        F_L1,
+        F_L2,
+        F_R1,
+        F_S1,
+        F_SZ1
+      } = grid.cells["Names"];
+
+      return WENNS(
+        F_L1 === A_JN2,
+        E_A1,
+        UND(F_R1 <= A_R1, F_S1 === A_S1, F_F1 === A_JN1),
+        E_A2,
+        UND(F_R1 <= A_R1, F_S1 === A_S1, F_F1 === A_JN2),
+        E_A3,
+        UND(F_R1 <= A_R1, F_S1 === A_S2, F_F1 === A_JN1),
+        E_A4,
+        UND(F_R1 <= A_R1, F_S1 === A_S2, F_F1 === A_JN2),
+        E_A5,
+        UND(F_R1 <= A_R1, F_S1 === A_S3),
+        E_A6,
+        UND(F_R1 > A_R1, F_S1 === A_S1),
+        E_A6,
+        UND(F_R1 > A_R1, F_SZ1 === A_SV1),
+        E_A9,
+        UND(F_R1 > A_R1, F_S1 === A_S2, F_SZ1 === A_SV2, F_L2 === A_JN1),
+        E_A8,
+        UND(F_R1 > A_R1, F_SZ1 === A_SV2, F_L2 === A_JN2),
+        E_A10,
+        UND(F_R1 > A_R1, F_S1 === A_S2, F_SZ1 === A_SV3, F_L2 === A_JN1),
+        E_A7,
+        UND(F_R1 > A_R1, F_S1 === A_S2, F_SZ1 === A_SV3, F_L2 === A_JN2),
+        E_A11,
+        UND(F_R1 > A_R1, F_S1 === A_S3, F_SZ1 === A_SV2, F_L2 === A_JN1),
+        E_A8,
+        UND(F_R1 > A_R1, F_S1 === A_S3, F_SZ1 === A_SV3, F_L2 === A_JN1),
+        E_A8,
+        UND(F_R1 > A_R1, F_S1 === A_S3, F_SZ1 === A_SV3, F_L2 === A_JN2),
+        E_A11,
+        WAHR(),
+        " "
+      );
+    });
+  }
+
+
 }
+
---
+++ src/app/data-grid.ts
@@ -0,0 +1,219 @@
+/**
+ * Represents the contents of a cell in the DataGrid.
+ */
+export type CellContent = string | CellFunc | number;
+/**
+ * A function that can be used as the content of a cell in the DataGrid.
+ */
+export type CellFunc = (sheet: string, cell: string, grid: DataGrid) => any;
+
+export type Sheet = Record<string, CellContent>;
+
+/**
+ * A class representing a grid of data, similar to an Excel spreadsheet.
+ */
+export class DataGrid {
+  cells: Record<string, Sheet>;
+  results: Record<string, Record<string, any>>;
+  g: (sheet: string, cell: string) => number | string;
+
+  constructor() {
+    this.cells = {};
+    this.results = {};
+
+    this.g = this.getCell.bind(this);
+  }
+
+  static log(...l: any[]): void {
+    console.log('DataGrid', ...l)
+  }
+
+  /**
+   * Sets the content of the specified cell in the specified sheet.
+   */
+  setCell(sheet: string, cell: string, content: CellContent): void {
+    if (typeof this.cells[sheet] === 'undefined') {
+      this.cells[sheet] = {};
+    }
+
+    if (typeof content === 'string' && /^[0-9\-]*[.,]?[0-9Ee\-]+$/.test(content)) {
+      content = parseFloat(content.replace(/[.,]/, '.'));
+    }
+
+    this.cells[sheet][cell] = content;
+  }
+
+  /**
+   * Sets the content of a range of cells in the specified sheet.
+   */
+  setCells(sheet: string, from: string, to: string, content: CellContent[][] | CellContent): void {
+    const $x1 = this.COLUMN(from);
+    const $y1 = this.ROW(from);
+    const $x2 = this.COLUMN(to);
+    const $y2 = this.ROW(to);
+
+    if (Array.isArray(content)) {
+      for (let col = $x1; col <= $x2; col++) {
+        for (let row = $y1; row <= $y2; row++) {
+          const cell = DataGrid.index2cell(col, row);
+          this.setCell(sheet, cell, content[row - $y1][col - $x1] || 0);
+        }
+      }
+    } else {
+      for (let col = $x1; col <= $x2; col++) {
+        for (let row = $y1; row <= $y2; row++) {
+          const cell = DataGrid.index2cell(col, row);
+          this.setCell(sheet, cell, content);
+        }
+      }
+    }
+  }
+
+  /**
+   * Gets the content of the specified cell in the specified sheet.
+   */
+  getCell(sheet: string, cell: string): number | string {
+    this.cells[sheet] ??= {};
+    this.results[sheet] ??= {};
+
+    const cellContent = /*this.results[sheet][cell] ?? */this.cells[sheet][cell];
+
+    if (typeof cellContent === 'function') {
+      const functionResult = cellContent(sheet, cell, this);
+      this.results[sheet][cell] = functionResult;
+      //DataGrid.log(`${sheet}:${cell}  ${this.results[sheet][cell]}`);
+      return functionResult;
+    }
+    return cellContent || 0;
+  }
+
+  /**
+   * Gets the content of the specified cell in the specified sheetas a Number or NaN if it is not
+   */
+  getCellNumeric(sheet: string, cell: string): number {
+    const value = this.getCell(sheet, cell);
+    return (typeof value === 'number' || typeof value === 'bigint') ? value : NaN;
+  }
+
+  /**
+   * Clears the results cache.
+   */
+  clearResults() {
+    for (const i in this.results) {
+      this.results[i] = {};
+    }
+  }
+
+  /**
+   * Calculates the sum of the specified cells in the specified sheet.
+   */
+  SUM(sheet: string, cells: string[]): number {
+    return cells.reduce((sum, cell) => {
+      let add = this.getCell(sheet, cell);
+      if (typeof add === 'number') {
+        return sum + add;
+      }
+      return NaN;
+    }, 0);
+  }
+
+  // Static helper methods
+
+  static charCodeOfA = 'A'.charCodeAt(0);
+  static alphabetLength = 'Z'.charCodeAt(0) - DataGrid.charCodeOfA + 1;
+
+  /**
+   * Converts a column name to a numeric column index.
+   */
+  static columnNameToColNumber(cell: string): number {
+    let result = 0;
+    cell = cell.replace(/[^A-Z]/g, '');
+    for (let i = 0; i < cell.length; i++) {
+      result *= DataGrid.alphabetLength;
+      result += cell.charCodeAt(i) - DataGrid.charCodeOfA + 1;
+    }
+    return result;
+  }
+
+  /**
+   * Extracts the row number from a cell name.
+   */
+  static cellNameToRowNumber(cell: string): number {
+    return parseInt(cell.replace(/[^0-9]/g, ''));
+  }
+
+  /**
+   * Converts a numeric column index to an Excel-style column name.
+   */
+  static getExcelColumnName(number: number): string {
+    let sb = '';
+    let num = number - 1;
+    while (num >= 0) {
+      sb = String.fromCharCode((num % DataGrid.alphabetLength) + DataGrid.charCodeOfA) + sb;
+      num = Math.floor(num / DataGrid.alphabetLength) - 1;
+    }
+    return sb;
+  }
+
+  /**
+   * Shifts the character codes of a string by a specified amount.
+   */
+  static unshift(str: string, n: number = 0): string {
+    let result = '';
+    for (const c of str) {
+      result += String.fromCharCode(c.charCodeAt(0) - n);
+    }
+    return result;
+  }
+
+  /**
+   * Returns the name of the cell relative to a given cell by a specified offset.
+   */
+  static relCell(cell: string, x: number, y: number): string {
+    const $x = DataGrid.columnNameToColNumber(cell) + x;
+    const $y = DataGrid.cellNameToRowNumber(cell) + y;
+
+    return DataGrid.getExcelColumnName($x) + $y;
+  }
+
+  // Implementations of Excel functions
+  COLUMN(cell: string): number {
+    return DataGrid.columnNameToColNumber(cell);
+  }
+
+  ROW(cell: string): number {
+    return DataGrid.cellNameToRowNumber(cell);
+  }
+
+  /**
+   * Retrieves the value from a specified cell in a specified sheet using the INDEX function.
+   */
+  INDEX(sheet: string, cell1: string, cell2: string, row: number, col: number): any {
+    const $x = this.COLUMN(cell1) + (col - 1);
+    const $y = this.ROW(cell1) + (row - 1);
+
+    return this.getCell(sheet, DataGrid.index2cell($x, $y));
+  }
+
+  /**
+   * Converts a row and column index to an Excel-style cell name.
+   */
+  static index2cell(x: number, y: number): string {
+    return DataGrid.getExcelColumnName(x) + y;
+  }
+}
+
+export function UND(...conditions: boolean[]): boolean {
+  return conditions.every((condition) => condition);
+}
+
+export function WAHR(): boolean {
+  return true;
+}
+
+export function WENNS(...args: any[]): any {
+  for (let i = 0; i < args.length - 1; i += 2) {
+    if (args[i]) return args[i + 1];
+  }
+  return args[args.length - 1];
+}
