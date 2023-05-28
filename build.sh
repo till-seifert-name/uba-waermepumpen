d1dfe727339a9df3540cf56b09a0364fe3df4900
Till Seifert <me@till.seifert.name>
2023-05-28 17:52:30 +0200

    Add build script for domain nachhaltige-oeffentliche-pkw-beschaffung.de

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -e
 npm run build-dev
 rsync -avzO --ignore-times --checksum --delete-after --fuzzy ./dist/dbu-online-tool/ \
  till.seifert.name@till.seifert.name:public_html/projects/dbu-online-tool/
+
+npm run build
+rsync -avzO --ignore-times --checksum --delete-after --fuzzy ./dist/dbu-online-tool/ \
+ root@my-eroads.de:/var/www/vhosts/nachhaltige-oeffentliche-pkw-beschaffung.de/betatest/
