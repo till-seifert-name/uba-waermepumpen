95f3d144110a6a359834b6f5fbdcd658729e2612
Till Seifert <me@till.seifert.name>
2023-06-01 15:19:40 +0200

    Text changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     path: '',
     component: StartComponent,
     pathMatch: 'full',
-    title: 'DBU Online-Tool'
+    title: 'Pkw-Beschaffung'
   },
   {
     path: 'hintergrundinformationen',
