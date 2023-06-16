615bb0a25e585677ddca4d7d50ae071e28c1a570
Till Seifert <me@till.seifert.name>
2023-06-16 14:09:05 +0200

    Save the accordion state

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import {MatExpansionPanel} from "@angular/material/expansion";
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MatAccordion, MatExpansionPanel} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
-export class StartComponent {
+export class StartComponent implements OnInit, OnDestroy {
+
+  @ViewChild(MatAccordion) accordion?: MatAccordion;
+
+  /**
+   * Static field to hold the state of the accordoin
+   */
+  static accordionState: boolean[];
+
+  ngOnInit(): void {
+    if (StartComponent.accordionState) {
+      this.applyAccordionState(StartComponent.accordionState);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.accordion) {
+      StartComponent.accordionState = this.getAccordionState();
+    }
+  }
+
+  private getAccordionState(): boolean[] {
+    return this.accordion?._headers.map(header => header._isExpanded()) ?? [];
+  }
+
+  private applyAccordionState(state: boolean[]): void {
+    setTimeout(() => {
+      this.accordion?._headers.forEach((header, index) => {
+        if (header._isExpanded() != state[index])
+          header._toggle();
+      });
+    });
+  }
 
   scrollTo(panel: MatExpansionPanel) {
     panel._body.nativeElement.scrollIntoView({behavior: "smooth"});
