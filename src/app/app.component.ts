42fd5fe224bb304e3f94bfce48a4c2f35cb3cd83
Till Seifert <me@till.seifert.name>
2023-06-26 13:01:40 +0200

    Add Matomo tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSidenav} from "@angular/material/sidenav";
-import {Router} from "@angular/router";
+import {EventType, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
+
+// Helper for Matomo
+declare let _paq: any;
 
 @Component({
   selector: 'app-root',
@@ -21,6 +25,16 @@ export class AppComponent implements OnInit {
     this.router.events.subscribe(() => {
       if (this.sidenav?.opened) {
         this.sidenav.close();
+      }
+    });
+
+    // Track navigation with Matomo
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => 'type' in event && event.type === EventType.NavigationEnd)
+    ).subscribe(event => {
+      if (typeof (_paq) !== 'undefined') {
+        _paq.push(['setCustomUrl', event.urlAfterRedirects]);
+        _paq.push(['trackPageView']);
       }
     });
   }
